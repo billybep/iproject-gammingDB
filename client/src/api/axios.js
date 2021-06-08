e3f28773b78ab63d152479ece0ea550dbff7888c
billybep <billy.pesoth@gmail.com>
2021-06-08 08:44:22 +0700

    deploy aws-s3

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios'
-export default axios.create({ baseURL: 'http://localhost:3000' })
+// export default axios.create({ baseURL: 'http://localhost:3000' })
 // export default axios.create({ baseURL: 'https://gamming-db.herokuapp.com' })
+export default axios.create({ baseURL: 'http://3.83.162.45:3000' })
